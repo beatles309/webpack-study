ff6703c8979d3d674ca8e9e8beb68c7b9aaee7a7
jinhyang <beatles309@naver.com>
2021-01-26 02:54:31 +0900

    jest 기본 사용법 학습

---
+++ src/components/textInputField.vue
@@ -0,0 +1,16 @@
+<template>
+  <input
+    type='text'
+    @click.prevent="inputText">
+</template>
+
+<script>
+export default {
+  name: 'textInputField',
+  methods: {
+    inputText (text) {
+      this.$emit('inputText', text)
+    }
+  }
+}
+</script>
--- tests/unit/test.spec.js
+++ tests/unit/test.spec.js
@@ -1,13 +1,30 @@
-import { shallowMount } from '@vue/test-utils'
-import Test from '@/components/Test.vue'
-import axios from 'axios'
+import { getObj, isNumisFive } from '../../src/js/test'
+//import axios from 'axios'
 
-jest.mock('axios', () => ({
-  get: Promise.resolve('테스트 result')
-}))
+// jest.mock('axios', () => ({
+//   get: Promise.resolve('테스트 result')
+// }))
+//
+// it('axios mock test', () => {
+//   const wrapper = shallowMount(Test)
+//   wrapper.find('button').trigger('click')
+//   expect(wrapper.text()).toBe('테스트 result')
+// })
 
-it('axios mock test', () => {
-  const wrapper = shallowMount(Test)
-  wrapper.find('button').trigger('click')
-  expect(wrapper.text()).toBe('테스트 result')
+it('toEqual', () => {
+  expect(getObj('jh')).toEqual({
+    id: 1,
+    email: `jh@naver.com`
+  })
+})
+
+it('toBe', () => {
+  expect(getObj('jh')).toBe({
+    id: 1,
+    email: `jh@naver.com`
+  })
+})
+
+it('isTruthly', () => {
+  expect(isNumisFive(5)).toBeTruthy()
 })
